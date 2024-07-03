e40b2577f181cc768695df5ea71d3b062d961b54
Garvit Goyal <garvitgoyal058@gmail.com>
2024-07-04 01:12:05 +0530

    Sidebar Started

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,8 +3,18 @@ import "./sidebar.scss"
 const Sidebar = () => {
     return(
         <div className="sidebar">
-            <div className="top">logo</div>
-            <div className="center">list</div>
+            <div className="top">
+                <span className="logo">StalkCash</span>
+            </div>
+            <hr/>
+            <div className="center">
+                <ul>
+                    <li><span>Dashboard</span></li>
+                    <li><span>Dashboard</span></li>
+                    <li><span>Dashboard</span></li>
+                    <li><span>Dashboard</span></li>
+                </ul>
+            </div>
             <div className="bottom">color option</div>
         </div>
     )
