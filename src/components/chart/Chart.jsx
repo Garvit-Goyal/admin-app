d4f2b97ea9837ab0650fa8e80464e8095085633e
Garvit Goyal <garvitgoyal058@gmail.com>
2024-07-11 21:38:35 +0530

    Rechart Done (Can be styled more)

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,9 +1,48 @@
 import "./chart.scss"
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        Month: "January",
+        Total : 18000
+    },
+    {
+        Month: "February",
+        Total : 15000
+    },
+    {
+        Month: "March",
+        Total : 30000
+    },
+    {
+        Month: "April",
+        Total : 55000
+    },
+    {
+        Month: "May",
+        Total : 40000
+    },
+];
 
 const Chart = () => {
-    return(
+    return (
         <div className="chart">
-            Chart
+            <h1 className="title">LAST 6 MONTHS PROGRESS (REVENUE)</h1>
+            <ResponsiveContainer width="100%" aspect={2/0.8} className="responsiveChart">
+                <AreaChart width={730} height={250} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                        <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="rgb(0, 63, 198)" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="rgb(0, 63, 198)" stopOpacity={0.2} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="Month" stroke="black"/>
+                    <CartesianGrid strokeDasharray="1 1" opacity={0.1} stroke="black"/>
+                    <Tooltip />
+                    <Area type="monotone" dataKey="Total" stroke="rgb(0, 63, 198)" fillOpacity={1} fill="url(#total)" />
+                </AreaChart>
+            </ResponsiveContainer>
         </div>
     )
 }
