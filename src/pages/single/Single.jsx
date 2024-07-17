dc235c6b1b28f794add903b8c3331bbb2d8fee25
Garvit Goyal <garvitgoyal058@gmail.com>
2024-07-18 02:31:56 +0530

    Users List - Datatable - Done

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,7 @@
 const Single = () => {
     return(
         <div className="single">
-            <h1 className="title">Login</h1>
+            <h1 className="title">SINGLE</h1>
         </div>
     )
 }
