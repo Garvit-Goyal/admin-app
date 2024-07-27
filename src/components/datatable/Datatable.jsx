23274852032faca0ae57b6eafc674683bece8851
Garvit Goyal <garvitgoyal058@gmail.com>
2024-07-27 21:03:42 +0530

    Fetch Data - Initialise (Datatable)

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,25 +1,69 @@
-import "./datatable.scss"
-import { userRows, userColumns } from "../../datatablesource";
-import { DataGrid } from '@mui/x-data-grid';
-import { Height, WidthFull } from "@mui/icons-material";
-
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+    collection,
+    getDocs,
+    deleteDoc,
+    doc,
+    onSnapshot,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
-const Datatable = ({headTitle}) => {
+const Datatable = ({ headTitle }) => {
+    const [data, setData] = useState([]);
 
-    const actionColumn = [{
-        field: "action", headerName: "Action", width: 200, renderCell: () => {
-            return (
-                <div className="cellAction">
-                    <Link to={"/users/test"} style={{ textDecoration: "none", color: "black" }}>
-                        <div className="viewButton">View</div>
-                    </Link>
-                    <div className="deleteButton">Delete</div>
-                </div>
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                let list = []
+                const querySnapshot = await getDocs(collection(db, "users"));
+                querySnapshot.forEach((doc) => {
+                    list.push({id: doc.id, ...doc.data()})
+                });
+                setData(list)
+            }
+            catch(error){
+                console.log(error)
+            }
 
-            );
+        };
+        fetchData()
+    }, []);
+
+    const handleDelete = async (id) => {
+        try {
+            await deleteDoc(doc(db, "users", id));
+            setData(data.filter((item) => item.id !== id));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const actionColumn = [
+        {
+            field: "action",
+            headerName: "Action",
+            width: 200,
+            renderCell: (params) => {
+                return (
+                    <div className="cellAction">
+                        <Link to="/users/test" style={{ textDecoration: "none" }}>
+                            <div className="viewButton">View</div>
+                        </Link>
+                        <div
+                            className="deleteButton"
+                            onClick={() => handleDelete(params.row.id)}
+                        >
+                            Delete
+                        </div>
+                    </div>
+                );
+            },
         },
-    },];
+    ];
 
     return (
         <div className="datatable">
@@ -29,7 +73,7 @@ const Datatable = ({headTitle}) => {
                 </div>
             </Link>
             <DataGrid
-                rows={userRows}
+                rows={data}
                 columns={userColumns.concat(actionColumn)}
                 initialState={{
                     pagination: {
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,70 +1,58 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Timestamp, addDoc, collection, doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { auth, db, storage } from "../../firebase";
 
-import "./new.scss"
+import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
-
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
 
 const New = ({ input, title }) => {
-    const [file, setFile] = useState("")
-    const [data, setData] = useState({})
-    const [perc, setPerc] = useState(null)
-
+    const [file, setFile] = useState(null);
+    const [data, setData] = useState({});
+    const [perc, setPerc] = useState(null);
 
     useEffect(() => {
         const uploadFile = () => {
-            const name = new Date().getTime() + file.name
+            const name = new Date().getTime() + file.name;
             const storageRef = ref(storage, name);
-
             const uploadTask = uploadBytesResumable(storageRef, file);
 
-            uploadTask.on('state_changed',
-                (snapshot) => {
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log('Upload is ' + progress + '% done');
-                    setPerc(progress)
-                },
-                (error) => {
-                    console.log(error)
-                },
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        setData((prev)=>({...prev, img: downloadURL}))
-                    });
-                }
-            );
-        }
-        file && uploadFile();
-    }, [file])
+            uploadTask.on('state_changed', (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setPerc(progress);
+                console.log('Upload is ' + progress + '% done');
+            }, (error) => {
+                console.error('Error during file upload:', error);
+            }, () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setData((prev) => ({ ...prev, img: downloadURL }));
+                });
+            });
+        };
+        if (file) uploadFile();
+    }, [file]);
 
     const handleInput = (e) => {
-        e.preventDefault()
-        const id = e.target.id;
-        const value = e.target.value;
-
-        setData({ ...data, [id]: value })
-    }
+        e.preventDefault();
+        const { id, value } = e.target;
+        setData((prev) => ({ ...prev, [id]: value }));
+    };
 
     const handleAdd = async (e) => {
-        e.preventDefault()
-
+        e.preventDefault();
         try {
-            const res = await createUserWithEmailAndPassword(auth, data.email, data.password)
+            const res = await createUserWithEmailAndPassword(auth, data.email, data.password);
             await setDoc(doc(db, "users", res.user.uid), {
                 ...data,
                 timeStamp: serverTimestamp()
             });
+        } catch (err) {
+            console.error('Error adding user:', err);
         }
-        catch (err) {
-            console.log(err)
-        }
-    }
-
+    };
 
     return (
         <div className="new">
@@ -75,7 +63,10 @@ const New = ({ input, title }) => {
                 </div>
                 <div className="bottom">
                     <div className="left">
-                        <img src={file ? URL.createObjectURL(file) : ""} alt="" /> 
+                        <img
+                            src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
+                            alt=""
+                        />
                     </div>
                     <div className="right">
                         <form onSubmit={handleAdd}>
@@ -83,21 +74,33 @@ const New = ({ input, title }) => {
                                 <label htmlFor="file">
                                     UPLOAD IMAGE: <DriveFolderUploadOutlinedIcon className="icon" />
                                 </label>
-                                <input type="file" id="file" onChange={(e) => setFile(e.target.files[0])} style={{ display: "none" }} />
+                                <input
+                                    type="file"
+                                    id="file"
+                                    onChange={(e) => setFile(e.target.files[0])}
+                                    style={{ display: "none" }}
+                                />
                             </div>
                             {input.map((input) => (
                                 <div className="formInput" key={input.id}>
                                     <label>{input.label}</label>
-                                    <input id={input.id} type={input.type} placeholder={input.placeholder} onChange={handleInput} />
+                                    <input
+                                        id={input.id}
+                                        type={input.type}
+                                        placeholder={input.placeholder}
+                                        onChange={handleInput}
+                                    />
                                 </div>
                             ))}
-                            <button disabled={perc!==null && perc<100} type="submit">SUBMIT</button>
+                            <button disabled={perc !== null && perc < 100} type="submit">
+                                SUBMIT
+                            </button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default New
+export default New;
